e2349d0eaa89577fc3925d2b46045280203bca46
AsipaBurhannudin <asipaburhannurdin@gmail.com>
2023-03-27 12:12:51 +0700

    Penambahan Countrys
    Penambahan CRUD untuk Countrys

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -20,7 +20,17 @@ public class Program
             Console.WriteLine("Connection Failed : " + e);
         }*/
 
-        GetAllRegion();
+        //GetAllRegion();
+        //GetByIdRegions(2);
+        //InsertRegions("Region Baru");
+        //UpdateRegions(5, "Region new");
+        //DeleteRegions(6);       
+
+        //GetAllCountrys();
+        //GetByIdCountrys("AR");
+        //InsertCountrys("ZX" , "Zhasx" , 2);
+        //UpdateCountrys("ZM", "Zambia");
+        //DeleteCountrys
     }
 
     //GET ALL
@@ -54,7 +64,7 @@ public class Program
     }
 
     //Get BY ID : Regions
-    public static void GetById()
+    public static void GetByIdRegions(int id)
     {
         connection = new SqlConnection(connectionString);
 
@@ -75,17 +85,388 @@ public class Program
         if (reader.HasRows)
         {
             reader.Read();
+            
+                Console.WriteLine("Id : " + reader[0]);
+                Console.WriteLine("Name : " + reader[1]);
+            
+        } else
+        {
+            Console.WriteLine($"id = {id} is not found!");
+        }
+        reader.Close();
+        connection.Close();
+    }
+
+    //
+    // INSERT : Regions
+    public static void InsertRegions(string name)
+    {
+        connection = new SqlConnection(connectionString);
+        connection.Open();
+
+        SqlTransaction transaction = connection.BeginTransaction();
+
+        try
+        {
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = "Insert Into regions (name) Values (@name);";
+            command.Transaction = transaction;
+
+            SqlParameter pName = new SqlParameter();
+            pName.ParameterName = "@name";
+            pName.SqlDbType = System.Data.SqlDbType.VarChar;
+            pName.Value = name;
+            command.Parameters.Add(pName);
+
+            command.ExecuteNonQuery();
+
+            transaction.Commit();
+            Console.WriteLine("Insert Success!");
+            connection.Close();
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Something Wrong! : " + e.Message);
+            try
+            {
+                transaction.Rollback();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+
+    // UPDATE : Region
+    public static void UpdateRegions(int id, string name)
+    {
+        connection = new SqlConnection(connectionString);
+        connection.Open();
+
+        SqlTransaction transaction = connection.BeginTransaction();
+
+        try
+        {
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = "Update regions Set name = @name Where id = @id;";
+            command.Transaction = transaction;
+
+            SqlParameter pName = new SqlParameter();
+            pName.ParameterName = "@name";
+            pName.SqlDbType = System.Data.SqlDbType.VarChar;
+            pName.Value = name;
+            command.Parameters.Add(pName);
+
+            SqlParameter pId = new SqlParameter();
+            pId.ParameterName = "@id";
+            pId.SqlDbType = System.Data.SqlDbType.Int;
+            pId.Value = id;
+            command.Parameters.Add(pId);
+
+            int result = command.ExecuteNonQuery();
+            if (result > 0)
+            {
+                Console.WriteLine("Update Success!");
+            }
+            else
+            {
+                Console.WriteLine($"id = {id} is not found!");
+            }
+
+            transaction.Commit();
+            connection.Close();
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Something Wrong! : " + e.Message);
+            try
+            {
+                transaction.Rollback();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+
+    // DELETE : Region
+    public static void DeleteRegions(int id)
+    {
+        connection = new SqlConnection(connectionString);
+        connection.Open();
+
+        SqlTransaction transaction = connection.BeginTransaction();
+
+        try
+        {
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = "Delete From regions Where id = @id;";
+            command.Transaction = transaction;
+
+            SqlParameter pId = new SqlParameter();
+            pId.ParameterName = "@id";
+            pId.SqlDbType = System.Data.SqlDbType.Int;
+            pId.Value = id;
+            command.Parameters.Add(pId);
+
+            int result = command.ExecuteNonQuery();
+            if (result > 0)
+            {
+                Console.WriteLine("Delete Success!");
+            }
+            else
+            {
+                Console.WriteLine($"id = {id} is not found!");
+            }
+
+            transaction.Commit();
+            connection.Close();
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Something Wrong! : " + e.Message);
+            try
+            {
+                transaction.Rollback();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+
+    // GET ALL : Countrys
+    public static void GetAllCountrys()
+    {
+        // Membuat instance SQL Server Connection
+        connection = new SqlConnection(connectionString);
+
+        // Membuat instance SQL Command
+        SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "Select * From countrys;";
+
+        connection.Open();
+        using SqlDataReader reader = command.ExecuteReader();
+        if (reader.HasRows)
+        {
+            while (reader.Read())
             {
                 Console.WriteLine("Id : " + reader[0]);
                 Console.WriteLine("Name : " + reader[1]);
+                Console.WriteLine("Region : " + reader[2]);
                 Console.WriteLine();
             }
         }
         else
         {
-            Console.WriteLine("Region is Empty");
+            Console.WriteLine("Countrys is Empty!");
         }
         reader.Close();
         connection.Close();
+    }
+
+    // GET BY ID : Countrys
+    public static void GetByIdCountrys(string id)
+    {
+        // Membuat instance SQL Server Connection
+        connection = new SqlConnection(connectionString);
+
+        // Membuat instance SQL Command
+        SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "Select * From countrys Where id = @id;";
+
+        // Membuat instance SQL Parameter
+        SqlParameter pId = new SqlParameter();
+        pId.ParameterName = "@id";
+        pId.SqlDbType = System.Data.SqlDbType.VarChar;
+        pId.Value = id;
+        command.Parameters.Add(pId);
+
+        connection.Open();
+        using SqlDataReader reader = command.ExecuteReader();
+        if (reader.HasRows)
+        {
+            reader.Read();
+
+            Console.WriteLine("Id : " + reader[0]);
+            Console.WriteLine("Name : " + reader[1]);
+            Console.WriteLine("Region : " + reader[2]);
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine($"id = {id} is not found!");
+        }
+        reader.Close();
+        connection.Close();
+    }
+
+    // INSERT : Countrys
+    public static void InsertCountrys(string id, string name, int region)
+    {
+        connection = new SqlConnection(connectionString);
+        connection.Open();
+
+        SqlTransaction transaction = connection.BeginTransaction();
+
+        try
+        {
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = "Insert Into countrys (id,name,region) Values (@id,@name,@region);";
+            command.Transaction = transaction;
+
+            SqlParameter pId = new SqlParameter();
+            pId.ParameterName = "@id";
+            pId.SqlDbType = System.Data.SqlDbType.VarChar;
+            pId.Value = id;
+            command.Parameters.Add(pId);
+
+            SqlParameter pName = new SqlParameter();
+            pName.ParameterName = "@name";
+            pName.SqlDbType = System.Data.SqlDbType.VarChar;
+            pName.Value = name;
+            command.Parameters.Add(pName);
+
+            SqlParameter pRegion = new SqlParameter();
+            pRegion.ParameterName = "@region";
+            pRegion.SqlDbType = System.Data.SqlDbType.Int;
+            pRegion.Value = region;
+            command.Parameters.Add(pRegion);
+
+            command.ExecuteNonQuery();
+
+            transaction.Commit();
+            Console.WriteLine("Insert Success!");
+            connection.Close();
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Something Wrong! : " + e.Message);
+            try
+            {
+                transaction.Rollback();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+
+    // UPDATE : Countrys
+    public static void UpdateCountrys(string id, string name)
+    {
+        connection = new SqlConnection(connectionString);
+        connection.Open();
+
+        SqlTransaction transaction = connection.BeginTransaction();
+
+        try
+        {
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = "Update countrys Set name = @name Where id = @id;";
+            command.Transaction = transaction;
+
+            SqlParameter pName = new SqlParameter();
+            pName.ParameterName = "@name";
+            pName.SqlDbType = System.Data.SqlDbType.VarChar;
+            pName.Value = name;
+            command.Parameters.Add(pName);
+
+            SqlParameter pId = new SqlParameter();
+            pId.ParameterName = "@id";
+            pId.SqlDbType = System.Data.SqlDbType.VarChar;
+            pId.Value = id;
+            command.Parameters.Add(pId);
+
+            int result = command.ExecuteNonQuery();
+            if (result > 0)
+            {
+                Console.WriteLine("Update Success!");
+            }
+            else
+            {
+                Console.WriteLine($"id = {id} is not found!");
+            }
+
+            transaction.Commit();
+            connection.Close();
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Something Wrong! : " + e.Message);
+            try
+            {
+                transaction.Rollback();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+
+    // DELETE : Countrys
+    public static void DeleteCountrys(int id)
+    {
+        connection = new SqlConnection(connectionString);
+        connection.Open();
+
+        SqlTransaction transaction = connection.BeginTransaction();
+
+        try
+        {
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = "Delete From countrys Where id = @id;";
+            command.Transaction = transaction;
+
+            SqlParameter pId = new SqlParameter();
+            pId.ParameterName = "@id";
+            pId.SqlDbType = System.Data.SqlDbType.Int;
+            pId.Value = id;
+            command.Parameters.Add(pId);
+
+            int result = command.ExecuteNonQuery();
+            if (result > 0)
+            {
+                Console.WriteLine("Delete Success!");
+            }
+            else
+            {
+                Console.WriteLine($"id = {id} is not found!");
+            }
+
+            transaction.Commit();
+            connection.Close();
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Something Wrong! : " + e.Message);
+            try
+            {
+                transaction.Rollback();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
     }
 }
